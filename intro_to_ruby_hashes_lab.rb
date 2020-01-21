aeeaf7f35c0bd09ae2dd37f28973517f0a8c5eb0
erocknine <echeungsk@gmail.com>
2020-01-21 21:21:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,10 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator[:key] = value
-  my_hash_creator
+  hash = {
+    key => value
+  }
+  hash
 end
 
 def read_from_hash(hash, key)
